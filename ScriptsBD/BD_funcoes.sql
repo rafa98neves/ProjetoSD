6da184f2a93008c4bf94a928802f5756881f15b2
nunespedro2323 <nunespedro2323@gmail.com>
2018-11-28 22:04:41 +0000

    Cenas

--- ScriptsBD/BD_funcoes.sql
+++ ScriptsBD/BD_funcoes.sql
@@ -1,3 +1,5 @@
+
+
 --Criar Genero
 INSERT INTO dbo.Genre
 (
@@ -170,7 +172,7 @@ VALUES
 	'@info5'
 )
 
-IF @info5 < 0
+IF '@info5' < 0
 BEGIN
 	INSERT INTO dbo.Music
 	(
@@ -192,79 +194,188 @@ BEGIN
 	)
 END
 
+--Obter detalhes de um Álbum
+--@Nome = Nome do Album
+	--Obter Cabeçalho de um Album
+	SELECT AlbInf.Name, AlbInf.Year
+	FROM dbo.AlbumInfo AlbInf
+	WHERE AlbInf.AlbumInfoID = (SELECT AlbInf.AlbumInfoID
+			FROM dbo.AlbumInfo AlbInf
+			WHERE AlbInf.Name = '@Nome')
 
---Acrescentar detalhes de um alvo
+			
+	--Obter Artistas de todas as músicas presentes num albúm
+	SELECT Art.Name
+	FROM dbo.Artist Art
+	WHERE Art.ArtistID IN (
+			SELECT Mus.Artist
+			FROM dbo.Music Mus
+			WHERE Mus.MusicInfo IN ( 
+				SELECT MusInf.MusicInfoID
+				FROM dbo.MusicInfo MusInf, dbo.Album Alb
+				WHERE Alb.AlbumInfo = (SELECT AlbInf.AlbumInfoID
+					FROM dbo.AlbumInfo AlbInf
+					WHERE AlbInf.Name = '@Nome') 
+				AND Alb.Music = MusInf.MusicInfoID
+				)
+			)
+
+			
+	--Obter nomes das músicas presentes num album
+	SELECT MusInf.Titulo
+	FROM dbo.MusicInfo MusInf, dbo.Album Alb
+	WHERE Alb.AlbumInfo = (SELECT AlbInf.AlbumInfoID
+			FROM dbo.AlbumInfo AlbInf
+			WHERE AlbInf.Name = '@Nome') 
+		AND Alb.Music = MusInf.MusicInfoID
+		
+		
+	--Obter avaliações inseridas pelos Users relacionadas com o Álbum em questão
+	SELECT Usr.username, Rev.Score, Rev.Comment
+	FROM dbo.Review Rev, dbo.Users Usr
+	WHERE Rev.UsernameID = Usr.UserID AND Rev.Album = (SELECT AlbInf.AlbumInfoID
+			FROM dbo.AlbumInfo AlbInf
+			WHERE AlbInf.Name = '@Nome')
+		
+
+--Obter detalhes de uma música
+--@Nome = Nome da Música a pesquisar
+
+	--Obter cabeçalho de uma música
+	SELECT MusInf.Titulo, Gen.Name
+	FROM dbo.MusicInfo MusInf, dbo.Genre Gen
+	WHERE MusInf.MusicInfoID = (SELECT MusInf.MusicInfoID
+		FROM dbo.MusicInfo MusInf
+		WHERE MusInf.Titulo LIKE '%'+'@Nome'+'%')
+	AND MusInf.Genero = Gen.GenreID
+	
+	--Obter artistas de uma música
+	SELECT Art.Name
+	FROM dbo.Artist Art, dbo.Music Mus
+	WHERE Mus.MusicInfo = (SELECT MusInf.MusicInfoID
+		FROM dbo.MusicInfo MusInf
+		WHERE MusInf.Titulo LIKE '%'+'@Nome'+'%') 
+	AND Mus.Artist = Art.ArtistID
+
+	
+--Obter detalhes de um género
+--@Nome = Nome do genero a pesquisar
+
+	--Obter cabeçalho do genero
+	SELECT Gen.Name
+	FROM dbo.Genre Gen
+	WHERE Gen.Name LIKE '%'+'@Nome'+'%'
+	
+	--Obter músicas que têm este genero definido
+	SELECT MusInf.Titulo
+	FROM dbo.MusicInfo MusInf
+	WHERE MusInf.Genero = (SELECT Gen.GenreID
+		FROM dbo.Genre Gen
+		WHERE Gen.Name LIKE '%'+'@Nome'+'%')
+	
+	
+--Obter detalhes de um artista
+--@Nome = Nome do artista a pesquisar
+
+	--
+	SELECT ArtInf.Name, ArtInf.Idade
+	FROM dbo.Artist ArtInf
+	WHERE ArtInf.Name LIKE '%'+'@Nome'+'%'
+		
+		
+-- GET Albuns de um Artista
+SELECT AlbInf.Name
+FROM dbo.AlbumInfo AlbInf, dbo.Album Alb
+WHERE AlbInf.AlbumInfoID = Alb.AlbumInfo AND Alb.Music IN (
+	SELECT DISTINCT Mus.MusicInfo
+	FROM dbo.Music Mus
+	WHERE Mus.Artist = ( SELECT ArtInf.ArtistID
+		FROM dbo.Artist ArtInf
+		WHERE ArtInf.Name LIKE '%'+'@Nome'+'%'
+		)
+	)
+GROUP BY AlbInf.Name
+
+
+--Criar informação de um alvo
 --@tipo = tipo de alvo (album,musica,..)
 --@nome = nome do alvo
 --@info e @info2 = Outras informações
 
-IF @tipo = 'artista'
+IF '@tipo' = 'artista'
 	BEGIN
-		IF @info = 'true'
-			BEGIN
-				SET @aux = 1;
-			END
-		IF @info = 'false'
-			BEGIN
-				SET @aux = 0;
-			END
-		
 		INSERT INTO dbo.Artist
 		(
 			Name,
-			Composer
+			Idade
 		)
 		VALUES
 		(
-			@nome,
-			@aux
+			'@nome',
+			cast('@info' AS int)
 		)
 	END
-IF @tipo = 'album'
+IF '@tipo' = 'album'
 	BEGIN
 		INSERT INTO dbo.Albuminfo
 		(
 			Name,
-			Year
+			Year,
+			Description,
+			Editora
 		)
 		VALUES
 		(
-			@nome,
-			cast(@info AS int)
+			'@nome',
+			cast('@info' AS int),
+			'@info2',
+			'@info3'
 		)
 	END	
 	
-IF @tipo = 'musica'
+IF '@tipo' = 'musica'
 	BEGIN
 		INSERT INTO dbo.MusicInfo
 		(
 			Titulo,
-			Genero
+			Genero,
+			Duracao,
+			Letra,
+			BandaID
 		)
 		VALUES
 		(
-			@nome,
+			'@nome',
 			(SELECT Findgen.GenreID
 				FROM dbo.Genre Findgen
-				WHERE Findgen.Name = @info2),
+				WHERE Findgen.Name = '@info2'),
+			'@info3',
+			'@info4',
+			'@info5'
 		)
 		
 		
-		
+		if('@info5' < 0) --Ou seja se não pertencer a nenhuma banda.
+		BEGIN
 		INSERT INTO dbo.Music
 		(
 			Musicinfo,
-			Artist
+			Artist,
+			Musico,
+			Compositor
 		)
 		VALUES
 		(
 			(SELECT Findmusic.MusicInfoID
 				FROM dbo.MusicInfo Findmusic
-				WHERE Findmusic.Titulo = @nome),
+				WHERE Findmusic.Titulo = '@nome'),
 			(SELECT Findartist.ArtistID
 				FROM dbo.Artist Findartist
-				WHERE Findartist.Name = @info)
+				WHERE Findartist.Name = '@info'),
+			'@aux', --Boolean: 0 se o artista em questão não for o músico da mesma, 1 caso contrário
+			'@aux2' --Boolean: 1 se o artista for Compositor da mesma, 1 caso contrário
 		)
+		END
 	END	
 --Returnar todos os generos existentes
 
@@ -277,7 +388,7 @@ SELECT Gen.Name FROM dbo.Genre Gen
 
 SELECT UsersTa.UserID, UsersTa.admin 
 	FROM dbo.Users UsersTa 
-	WHERE UsersTa.username = @User AND UsersTa.password = @Password;
+	WHERE UsersTa.username = '@User' AND UsersTa.password = '@Password';
 	
 	
 	
@@ -293,9 +404,9 @@ INSERT INTO dbo.Historicoo
 	)
 	VALUES
 	(
-		@Tipo,
-		@Nome,
-		@UserID
+		'@Tipo',
+		'@Nome',
+		'@UserID'
 	)
 
 	
@@ -305,72 +416,78 @@ INSERT INTO dbo.Historicoo
 
 SELECT Notif.Message
 	FROM dbo.Notification Notif
-	WHERE Notif.UserID = @User;
+	WHERE Notif.UserID = '@User';
 
 DELETE FROM dbo.Notification
-	WHERE dbo.Notification.UserID = @User;
+	WHERE dbo.Notification.UserID = '@User';
 
 	
 	
 --Dar/retirar privilégios de editor a um utilizador
---@User = Id do utilizador
+--@User = username do utilizador ao qual
 
 SELECT Usr.UserID, Usr.admin
 	FROM dbo.Users Usr
-	WHERE Usr.username = @User
+	WHERE Usr.username = '@User'
 
-	IF Usr.admin = 0
+	IF (SELECT Usr.UserID, Usr.admin
+		FROM dbo.Users Usr
+		WHERE Usr.username = '@User') = 0
 	BEGIN
-		SET Usr.admin = 1
+		UPDATE dbo.Users
+		SET admin = 1
+		WHERE username = '@User'
 	END
-
-	IF Usr.admin = 1
+	ELSE
 	BEGIN
-		SET Usr.admin = 0
+		UPDATE dbo.Users
+		SET admin = 0
+		WHERE username = '@User'
 	END
-
-	UPDATE dbo.Users
-	SET admin = Usr.admin
-	WHERE UserID = @UserID
 
 --Registar Utilizador
 --@User = username inserido pelo utilizador
 --@Password = password inserida pelo utilizador
 
 IF (SELECT COUNT(*) FROM dbo.Users UsersTa 
-	WHERE UsersTa.username = @User) = 0
+	WHERE UsersTa.username = '@User') = 0
+BEGIN
+	IF (SELECT COUNT(*) FROM dbo.Users ) = 0
 	BEGIN
-		IF (SELECT COUNT(*) FROM dbo.Users ) = 0
-			BEGIN
-				INSERT INTO dbo.Users
-				(
-					username,
-					password,
-					admin
-				)
-				VALUES
-				(
-					@User,
-					@Password,
-					1
-				)
-			END
-		IF (SELECT COUNT(*) FROM dbo.Users) > 0
-			BEGIN
-				INSERT INTO dbo.Users
-				(
-					username,
-					password,
-					admin
-				)
-				VALUES
-				(
-					@User,
-					@Password,
-					0
-				)
-			END
-	END	
+		INSERT INTO dbo.Users
+		(
+			username,
+			password,
+			admin
+		)
+		VALUES
+		(
+			'@User',
+			'@Password',
+			1
+		)
+	END
+	IF (SELECT COUNT(*) FROM dbo.Users) > 0
+	BEGIN
+		INSERT INTO dbo.Users
+		(
+			username,
+			password,
+			admin
+		)
+		VALUES
+		(
+			'@User',
+			'@Password',
+			0
+		)
+	END
+
+	SELECT UsersTa.UserID, UsersTa.admin 
+		FROM dbo.Users UsersTa 
+		WHERE UsersTa.username = '@User' AND UsersTa.password = '@Password';
+
+END	
 
 	
 	
@@ -378,64 +495,64 @@ IF (SELECT COUNT(*) FROM dbo.Users UsersTa
 --@Tipo = Tipo de alvo (Musica ou album)
 --@Nome = Nome do alvo
 --@AdicionaNome = Nome do artista (??)
-IF @Tipo = 'musica'
-	BEGIN
-		DELETE FROM dbo.Music
-		WHERE MusicInfo = (SELECT MusInf.MusicInfoID
-			FROM dbo.MusicInfo MusInf
-			WHERE MusInf.Titulo =  @Nome)
-		AND Artist = (SELECT Art.ArtistID
-			FROM dbo.Artist Art
-			WHERE @AdicionaNome = Art.Name)
-	END
+IF '@Tipo' = 'musica'
+BEGIN
+	DELETE FROM dbo.Music
+	WHERE MusicInfo = (SELECT MusInf.MusicInfoID
+		FROM dbo.MusicInfo MusInf
+		WHERE MusInf.Titulo =  '@Nome')
+	AND Artist = (SELECT Art.ArtistID
+		FROM dbo.Artist Art
+		WHERE '@AdicionaNome' = Art.Name)
+END
 
-IF @Tipo = 'album'
-	BEGIN
-		DELETE FROM dbo.Album
-		WHERE AlbumInfo = (SELECT AlbInf.AlbumInfoID
-			FROM dbo.AlbumInfo AlbInf
-			WHERE @Nome = AlbInf.Name)
-		AND Music = (SELECT MusInf.MusicInfoID
-			FROM dbo.MusicInfo MusInf
-			WHERE @AdicionaNome = MusInf.Titulo)
-	END
+IF '@Tipo' = 'album'
+BEGIN
+	DELETE FROM dbo.Album
+	WHERE AlbumInfo = (SELECT AlbInf.AlbumInfoID
+		FROM dbo.AlbumInfo AlbInf
+		WHERE '@Nome' = AlbInf.Name)
+	AND Music = (SELECT MusInf.MusicInfoID
+		FROM dbo.MusicInfo MusInf
+		WHERE '@AdicionaNome' = MusInf.Titulo)
+END
 
 	
 --Procurar
 --@TipoSearch = Tipo do alvo a ser procurado (album,musica,..)
 --@Nome = Nome do alvo
 
-IF @TipoSearch = 'album'
-	BEGIN
-		SELECT AlbInf.Name
-		FROM dbo.AlbumInfo AlbInf
-		WHERE AlbInf.Name LIKE '%'+@Nome+'%'
-	END
+IF '@TipoSearch' = 'album'
+BEGIN
+	SELECT AlbInf.Name
+	FROM dbo.AlbumInfo AlbInf
+	WHERE AlbInf.Name LIKE '%'+'@Nome'+'%'
+END
 
-IF @TipoSearch = 'musica'
+IF '@TipoSearch' = 'musica'
 	BEGIN
 		SELECT MusInf.Titulo
 		FROM dbo.MusicInfo MusInf
-		WHERE MusInf.Titulo LIKE '%'+@Nome+'%'
+		WHERE MusInf.Titulo LIKE '%'+'@Nome'+'%'
 	END
 
-IF @TipoSearch = 'genero'
+IF '@TipoSearch' = 'genero'
 	BEGIN
 		SELECT GenInf.Name
 		FROM dbo.Genre GenInf
-		WHERE GenInf.Name LIKE '%'+@Nome+'%'
+		WHERE GenInf.Name LIKE '%'+'@Nome'+'%'
 	END
 
-IF @TipoSearch = 'artista'
+IF '@TipoSearch' = 'artista'
 	BEGIN
 		SELECT ArtInf.Name
 		FROM dbo.Artist ArtInf
-		WHERE ArtInf.Name LIKE '%'+@Nome+'%'
+		WHERE ArtInf.Name LIKE '%'+'@Nome'+'%'
 	END
 
 	
 --Fazer Critica a um album	
---@User = utilizador que fez a critica
+--@User = ID do utilizador que fez a critica
 --@Album = nome do album a ser criticado
 --@Critica = Critica feita pelo utilizador
 --@PONT = pontuação dada pelo utilizador	
@@ -449,10 +566,10 @@ INSERT INTO dbo.Review
 )
 VALUES
 (
-	@User,
+	'@User',
 	(SELECT Findalbum.AlbumInfoID
 		FROM dbo.AlbumInfo Findalbum
-		WHERE Findalbum.Name = @Album),
-	@Critica,
-	@PONT
+		WHERE Findalbum.Name = '@Album'),
+	'@Critica',
+	'@PONT'
 )
